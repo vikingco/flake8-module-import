d48e43c8b7de87dfd8d82891a2119c90ea8f7667
Niels Van Och <niels.vanoch@gmail.com>
2017-02-14 14:38:02 +0100

    Initial version

---
+++ README.rst
@@ -0,0 +1,25 @@
+Flake8 module-level imports plugin
+==================================
+
+Check for module-level imports.
+
+This module provides a plugin for ``flake8``, the Python code checker.
+
+
+Installation
+------------
+
+You can install or upgrade ``flake8-module-imports`` with these commands::
+
+  $ pip install flake8-module-imports
+  $ pip install --upgrade flake8-module-imports
+
+
+Plugin for Flake8
+-----------------
+
+When both ``flake8`` and ``flake8-module-imports`` are installed, the plugin is
+available in ``flake8``::
+
+    $ flake8 --version
+    2.0 (pep8: 1.4.5, flake8-module-imports: 1.0, pyflakes: 0.6.1)
---
+++ flake8_module_imports.py
@@ -0,0 +1,47 @@
+import ast
+import tokenize
+
+from sys import stdin
+
+__version__ = '1.0'
+
+MODULE_IMPORTS_ERROR_CODE = 'T005'
+MODULE_IMPORTS_ERROR_MESSAGE = 'module-level import found'
+
+
+class ModuleImportsChecker(object):
+    name = 'flake8-module-imports'
+    version = __version__
+    ignores = ()
+
+    def __init__(self, tree, filename='(none)'):
+        self.tree = tree
+        self.filename = (filename == 'stdin' and stdin) or filename
+
+    def run(self):
+        # Get lines to ignore
+        if self.filename == stdin:
+            noqa = _get_noqa_lines(self.filename)
+        else:
+            with open(self.filename, 'r') as file_to_check:
+                noqa = _get_noqa_lines(file_to_check.readlines())
+
+        # Run the actual check
+        errors = []
+        for node in ast.walk(self.tree):
+            if isinstance(node, ast.Import) and node.lineno not in noqa:
+                errors.append({
+                    'message': '{0} {1}: {2}'.format(MODULE_IMPORTS_ERROR_CODE, MODULE_IMPORTS_ERROR_MESSAGE, node.names[0].name),
+                    'line': node.lineno,
+                    'col': node.col_offset,
+                })
+
+        # Yield the found errors
+        for error in errors:
+            yield (error.get("line"), error.get("col"), error.get("message"), type(self))
+
+
+def _get_noqa_lines(code):
+    tokens = tokenize.generate_tokens(lambda L=iter(code): next(L))
+    return [token[2][0] for token in tokens if token[0] == tokenize.COMMENT and
+            (token[1].endswith('noqa') or (isinstance(token[0], str) and token[0].endswith('noqa')))]
